3c18b841ba643d1147711c46685a3f568cd794b2
MohmmedAslamRathore <aslamrathore61@gmail.com>
2024-08-25 21:54:35 +0530

    path reset and google login issue fixed

--- app/src/main/java/com/nexalogic/kisankavikas/MainActivity.java
+++ app/src/main/java/com/nexalogic/kisankavikas/MainActivity.java
@@ -2,25 +2,70 @@ package com.nexalogic.kisankavikas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     WebView webView;
+    public static final String USER_AGENT = "Mozilla/5.0 (Linux; Android 10; Mobile; rv:79.0) Gecko/79.0 Firefox/79.0";
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         webView = findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setUserAgentString(USER_AGENT);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+            webView.getSettings().getMixedContentMode();
+        }
+
+
+        progressBar = findViewById(R.id.progressBar);
+
+        // Set a WebViewClient to manage the loading events
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, android.graphics.Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                // Show the progress bar when the page starts loading
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // Hide the progress bar when the page finishes loading
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                // Hide the progress bar if an error occurs
+                progressBar.setVisibility(View.GONE);
+            }
+        });
 
         // Load a web page into the WebView
         webView.loadUrl("https://kisankavikas.com/");
+
+
 
 
 
